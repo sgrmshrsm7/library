8f73f5837148f46dc6143c2303cd187b928a25f1
sgrmshrsm7 <sgrmshrsm7@gmail.com>
2021-10-28 13:20:58 +0530

    faq and student home

--- src/Member/faq.js
+++ src/Member/faq.js
@@ -4,45 +4,48 @@ import React from "react";
 
 const faq_mem = () => {
     return (
-			<div>
-            <h1 class="faq-heading">FAQ'S</h1>
-            <div class="faq-container">
-            <div class="faq-one">
-                
-                <h1 class="faq-page">How much is the fine for each book if we fail to renew on time ?</h1>
-                
-                <div class="faq-body">
-                    <p>On each book student get a penalty of 10 INR on each period delay.</p>
-                </div>
-            </div>
-            
-            <div class="faq-two">
-                
-                <h1 class="faq-page">What site we can refer to for basic HTML course ?</h1>
-                
-                <div class="faq-body">
-                    <p>There are numerous sites but "W3schools" is prefered by most of the students.</p>
-                </div>
-            </div>
-            
-            <div class="faq-three">
-                
-            <h1 class="faq-page">What book is prefered for C Programming ?</h1>
-                
-                <div class="faq-body">
-                    <p>Let Us C by Yashavant Kanetkar.</p>
-                </div>
-            </div>
-        
-       
-        </div>
-        </div>
+        <div className="faqsection">
+            <h1 className="loginhead">FAQ'S</h1>
+            <div className="faq-container">
+                <div className="faq-one">
+                    <h1 className="faq-page">
+                        How much is the fine for each book if we fail to renew
+                        on time ?
+                    </h1>
 
+                    <div className="faq-body">
+                        <p>
+                            On each book student get a penalty of 10 INR on each
+                            period delay.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="faq-two">
+                    <h1 className="faq-page">
+                        What site we can refer to for basic HTML course ?
+                    </h1>
+
+                    <div className="faq-body">
+                        <p>
+                            There are numerous sites but "W3schools" is prefered
+                            by most of the students.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="faq-three">
+                    <h1 className="faq-page">
+                        What book is prefered for C Programming ?
+                    </h1>
+
+                    <div className="faq-body">
+                        <p>Let Us C by Yashavant Kanetkar.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
 export default faq_mem;
-
-
-
-
--- src/Member/home.js
+++ src/Member/home.js
@@ -1,74 +1,120 @@
 import React from "react";
-
-// import "./header_member_style.css";
+import { Link } from "react-router-dom";
 
 const home_mem = () => {
     return (
-        <div >
-			<form class='cd-form' method='GET' action='./home/search'>
-                <div className="loginhead">Issued books</div>
-				<div class='error-message' id='error-message'>
-						<p id='error'></p>
-				</div>
-				<table width='100%' cellpadding='10' cellspacing='10'>
-                    <tr>
-                            <th></th>
-                            <th>Name of book<hr></hr></th>
-                            <th>Edition<hr></hr></th>
-                            <th>Author<hr></hr></th>
-                            <th>Publication<hr></hr></th>
-                            <th>Due_Date<hr></hr></th>
-                    </tr>
+        <div className="studentHome">
+            <div className="loginhead">Issued books</div>
+            <div class="error-message" id="error-message">
+                <p id="error"></p>
+            </div>
+            <table width="100%" cellpadding="10" cellspacing="10">
+                <tr>
+                    <th></th>
+                    <th>
+                        Name of book<hr></hr>
+                    </th>
+                    <th>
+                        Edition<hr></hr>
+                    </th>
+                    <th>
+                        Author<hr></hr>
+                    </th>
+                    <th>
+                        Publication<hr></hr>
+                    </th>
+                    <th>
+                        Due_Date<hr></hr>
+                    </th>
+                </tr>
 
+                <tr>
+                    <td></td>
+                    <td>
+                        Programming with c++<br></br>
+                    </td>
+                    <td>
+                        2nd<br></br>
+                    </td>
+                    <td>
+                        Brian Kernighan<br></br>
+                    </td>
+                    <td>
+                        Prentice Hall<br></br>
+                    </td>
+                    <td>
+                        10-11-2021<br></br>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        Engineering Circuit Analysis<br></br>
+                    </td>
+                    <td>
+                        3rd<br></br>
+                    </td>
+                    <td>
+                        William H. Hayt<br></br>
+                    </td>
+                    <td>
+                        McGraw Hill Education<br></br>
+                    </td>
+                    <td>
+                        10-11-2021<br></br>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        Theory of Machines<br></br>
+                    </td>
+                    <td>
+                        3rd<br></br>
+                    </td>
+                    <td>
+                        J.K. Gupta, R.S. Khurmi, and RS Khurmi<br></br>
+                    </td>
+                    <td>
+                        S Chand and Co Ltd<br></br>
+                    </td>
+                    <td>
+                        10-11-2021<br></br>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        Mechanical metallurgy<br></br>
+                    </td>
+                    <td>
+                        4th<br></br>
+                    </td>
+                    <td>
+                        George Dieter<br></br>
+                    </td>
+                    <td>
+                        McGraw Hill Education<br></br>
+                    </td>
+                    <td>
+                        10-11-2021<br></br>
+                    </td>
+                </tr>
+            </table>
 
-                    <tr>
-                            <td></td>
-                            <td>Programming with c++<br></br></td>
-                            <td>2nd<br></br></td>
-                            <td>Brian Kernighan<br></br></td>
-                            <td>Prentice Hall<br></br></td>
-                            <td>10-11-2021<br></br></td>
-                    </tr>
-                    <tr>
-                            <td></td>
-                            <td>Engineering Circuit Analysis<br></br></td>
-                            <td>3rd<br></br></td>
-                            <td>William H. Hayt<br></br></td>
-                            <td>McGraw Hill Education<br></br></td>
-                            <td>10-11-2021<br></br></td>
-                    </tr>
-                    <tr>
-                            <td></td>
-                            <td>Theory of Machines<br></br></td>
-                            <td>3rd<br></br></td>
-                            <td>J.K. Gupta, R.S. Khurmi, and RS Khurmi<br></br></td>
-                            <td>S Chand and Co Ltd<br></br></td>
-                            <td>10-11-2021<br></br></td>
-                    </tr>
-                    <tr>
-                            <td></td>
-                            <td>Mechanical metallurgy<br></br></td>
-                            <td>4th<br></br></td>
-                            <td>George Dieter<br></br></td>
-                            <td>McGraw Hill Education<br></br></td>
-                            <td>10-11-2021<br></br></td>
-                    </tr>
-                    
-				</table>
-				<br /><br /><input type='submit' name='m_request' value='Request book' />
-                                
-                                <br/>
-                                <a href="/member/home/faq">FAQ</a>
-                                <br />
-                                <a href="/member/home/reissue">Reissue books</a>
-			</form>
-
-		</div>
+            <div className="studentButtons">
+                <Link to="/member/home/reissue">
+                    <div className="studHomeButton">Reissue books</div>
+                </Link>
+                <Link to="/member/home/search">
+                    <div className="studHomeButton">Search Book</div>
+                </Link>
+                <Link to="/member/home/faq">
+                    <div className="studHomeButton">FAQ</div>
+                </Link>
+            </div>
+        </div>
     );
 };
 
 export default home_mem;
-
-
-
-
