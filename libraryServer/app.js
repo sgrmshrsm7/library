45f22401c8ae6e7b367d90f47c991b9be974fbd3
sgrmshrsm7 <sgrmshrsm7@gmail.com>
2022-03-23 19:59:58 +0530

    search by image

--- libraryServer/app.js
+++ libraryServer/app.js
@@ -10,12 +10,15 @@ const port = process.env.PORT;
 
 let cors = require("cors");
 let unggah = require("express-fileupload");
-const fs = require("fs");
 
 // // Database connection
 // require("./db/conn");
 
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(
+    express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
+);
 
 app.use(cors());
 app.use(unggah());
